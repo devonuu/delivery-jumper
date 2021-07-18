98b37637205a401d903adad9e0cb8bbb3f3d61d5
Gunwoo Park <prayzz12@gmail.com>
2021-07-18 21:42:17 +0900

    feat: 프로젝트 생성

---
+++ src/main/java/com/deliveryjumper/DeliveryJumperApplication.java
@@ -0,0 +1,13 @@
+package com.deliveryjumper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryJumperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryJumperApplication.class, args);
+	}
+
+}
