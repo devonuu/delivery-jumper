8753b0010f993b1fd33b4a7003a1e4493f8d7b0f
Gunwoo Park <prayzz12@gmail.com>
2021-08-06 16:23:05 +0900

    feat: 상속 관계 해제
    spring security 적용으로 인한 인증 및 권한을 Role 클래스로 위임하고 상속 엔티티를 제거 하였습니다.

--- src/main/java/com/deliveryjumper/domain/Member.java
+++ src/main/java/com/deliveryjumper/domain/Member.java
@@ -1,6 +1,8 @@
 package com.deliveryjumper.domain;
 
+import javax.persistence.Column;
 import javax.persistence.DiscriminatorColumn;
+import javax.persistence.DiscriminatorValue;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
@@ -9,9 +11,13 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
+import javax.persistence.Transient;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 /**
@@ -25,21 +31,96 @@ import lombok.Setter;
 
 
 @Entity
-@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @Getter
 @Setter(value = AccessLevel.PRIVATE)
-@EqualsAndHashCode
-@DiscriminatorColumn
-public abstract class Member {
+@EqualsAndHashCode(callSuper=false)
+@NoArgsConstructor
+@AllArgsConstructor
+public class Member extends BaseTimeEntity{
     @Id
     @GeneratedValue
     private Long memberId;
-    private String loginId;
+    private String email;
     private String password;
+    private String name;
+    private String picture;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
 
     @Embedded
     private Address address;
 
     @Enumerated(EnumType.STRING)
     private MemberStatus memberStatus;
+
+    public Member update(String name, String picture){
+        this.name = name;
+        this.picture = picture;
+        return this;
+    }
+
+/*    *//**
+     * spring security에서 사용될 권한 정보를 가져오기 위해 DiscriminatorValue값을 가져온다.
+     *//*
+    @Transient //
+    public String getDecriminatorValue() {
+        return this.getClass().getAnnotation(DiscriminatorValue.class).value();
+    }*/
+
+    public void encodingPassword(String password){
+        this.password = password;
+    }
+
+    public static class Builder{
+        private String email;
+        private Role role;
+        private String password;
+        private String name = "";
+        private String picture = "";
+        private Address address = null;
+
+        public Builder email(String email){
+            this.email = email;
+            return this;
+        }
+
+        public Builder role(Role role){
+            this.role = role;
+            return this;
+        }
+
+        public Builder name(String name){
+            this.name = name;
+            return this;
+        }
+
+        public Builder picture(String picture){
+            this.picture = picture;
+            return this;
+        }
+
+        public Builder address(Address address){
+            this.address = address;
+            return this;
+        }
+
+        public Builder password(String password){
+            this.password = password;
+            return this;
+        }
+
+        public Member build(){
+            return new Member(this);
+        }
+    }
+
+    private Member(Builder builder){
+        email = builder.email;
+        password = builder.password;
+        name = builder.name;
+        picture = builder.picture;
+        address = builder.address;
+        role = builder.role;
+    }
 }
---
+++ src/main/java/com/deliveryjumper/domain/Role.java
@@ -0,0 +1,16 @@
+package com.deliveryjumper.domain;
+/**
+ * Project : delivery-jumper
+ * Created by gonuu
+ * Date : 2021-08-06
+ * Time : 오후 3:20
+ * Blog : http://devonuu.tistory.com
+ * Github : http://github.com/devonuu
+ */
+
+
+public enum Role {
+    ROLE_HOST,
+    ROLE_RIDER,
+    ROLE_CUSTOMER
+}
